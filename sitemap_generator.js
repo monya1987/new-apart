51f15b20d79c146774e77e2391ec801a6fb41767
monya1987@gmail.com <311505064z>
2020-06-13 11:11:54 +0300

    add

--- sitemap_generator.js
+++ sitemap_generator.js
@@ -1,10 +1,20 @@
 const sitemap = require('nextjs-sitemap-generator');
+const urls = require('./public/data/ulrs');
+
+const getRoutes = () => {
+    const routes = [];
+    urls.forEach((record) => {
+        routes.push('/'+record.url);
+    });
+    return routes;
+};
 
 sitemap({
     baseUrl: 'https://www.new-apart.od.ua',
     pagesDirectory: __dirname + "/pages",
     targetDirectory : 'public/',
     ignoredPaths: ['api'],
+    extraPaths: getRoutes(),
 });
 
 console.log(`✅ sitemap.xml generated!`);
